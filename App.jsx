e44c55638d962e48818bf21b42da2e1e86d57bfe
Bobbydrake132 <114181999+Bobbydrake132@users.noreply.github.com>
2024-10-30 15:34:22 -0600

    Add files via upload
    AdminDashBoard Update

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './Header'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='grid-container'>
+      <Header />
+      <Sidebar />
+      <Home />
+    </div>>
+  )
+}
+
+export default App
